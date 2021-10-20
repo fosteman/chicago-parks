f89cefd67df0c0e1f430b83ff43db2b13208c31b
Ky <kywildermuth@gmail.com>
2021-10-20 13:54:12 -0500

    Update Map to be functional component

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import { useState } from 'react'
 import ReactMapGL from 'react-map-gl'
 
 type MapViewport = {
@@ -9,26 +9,16 @@ type MapViewport = {
   zoom: number
 }
 
-export default class Map extends Component {
-  state = {
-    viewport: {},
-  }
+export default function Map() {
+  const [viewport, setViewport] = useState({})
 
-  onViewportChange = (viewport: MapViewport) => {
-    const { width, height, ...etc } = viewport
-    this.setState({ viewport: etc })
-  }
-
-  render() {
-    const { viewport } = this.state
-    return (
-      <ReactMapGL
-        width="100%"
-        height="100%"
-        {...viewport}
-        mapboxApiAccessToken="pk.eyJ1Ijoid2lsZGt5IiwiYSI6ImNrdXRzamJ3czA5OXYyb2xmcmVjcWlmaHgifQ.8M22Z9gqiVxCkFCik-lCjw"
-        onViewportChange={(viewport: MapViewport) => this.onViewportChange(viewport)}
-      />
-    )
-  }
+  return (
+    <ReactMapGL
+      mapboxApiAccessToken="pk.eyJ1Ijoid2lsZGt5IiwiYSI6ImNrdXRzamJ3czA5OXYyb2xmcmVjcWlmaHgifQ.8M22Z9gqiVxCkFCik-lCjw"
+      {...viewport}
+      width="100vw"
+      height="100vh"
+      onViewportChange={setViewport}
+    />
+  )
 }
