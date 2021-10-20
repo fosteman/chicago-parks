d16fa24dcb265c67e41ba5785650cb543db8b3ed
Ky <kywildermuth@gmail.com>
2021-10-20 14:09:36 -0500

    move mapbox api key to env file

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,23 +1,15 @@
 import { useState } from 'react'
 import ReactMapGL from 'react-map-gl'
 
-type MapViewport = {
-  width: string
-  height: string
-  latitude: number
-  longitude: number
-  zoom: number
-}
-
 export default function Map() {
   const [viewport, setViewport] = useState({})
 
   return (
     <ReactMapGL
-      mapboxApiAccessToken="pk.eyJ1Ijoid2lsZGt5IiwiYSI6ImNrdXRzamJ3czA5OXYyb2xmcmVjcWlmaHgifQ.8M22Z9gqiVxCkFCik-lCjw"
+      mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
       {...viewport}
-      width="100vw"
-      height="100vh"
+      width="100%"
+      height="100%"
       onViewportChange={setViewport}
     />
   )
