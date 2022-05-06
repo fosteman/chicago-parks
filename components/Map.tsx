d87fdd06c4a386f776b424fc25f572e71c3abb07
fosteman <timfosteman@gmail.com>
2022-05-06 08:00:33 -0400

    GETting parks from endpoint

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ReactMapGL from 'react-map-gl'
 
 export default function Map() {
   const [viewport, setViewport] = useState({})
 
+  const [allData, setAllData] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      '/api/parks'
+    )
+      .then(resp => resp.json())
+      .then(json => setAllData(json))
+      .catch(err => console.error('Could not load data', err)); // eslint-disable-line
+  }, []);
+
+
+  console.log(allData)
   return (
     <ReactMapGL
       mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
